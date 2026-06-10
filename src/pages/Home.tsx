b0f3c84f263ea4f886d721327e39c3a6df9adab5
Kevinx-star <kevinandrespiernagorda@gmail.com>
2026-06-10 01:54:15 -0500

    Se añade el correo electronico

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,7 @@ function Home({ usuario, onLogout }: Props) {
           <IonCardHeader>
 
             <IonCardTitle>
-              Bienvenido {usuario}
+              Bienvenido: {usuario}
             </IonCardTitle>
 
           </IonCardHeader>
